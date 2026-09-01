lovits/FINAgent

fix: bundle config/recursion/missing-key fixes
- dataflows/config: deepcopy + one-level dict merge so a partial
  set_config doesn't clobber sibling defaults
- graph: thread max_recur_limit from config to Propagator
- openai_client: name the missing env var in the API-key error

#788 #764 #680

tradingagents/dataflows/config.py
@@ -1,5 +1,7 @@
-import tradingagents.default_config as default_config
+from copy import deepcopy
 from typing import Dict, Optional
+
+import tradingagents.default_config as default_config
 
 # Use default config but allow it to be overridden
 _config: Optional[Dict] = None
@@ -9,22 +11,31 @@ def initialize_config():
     """Initialize the configuration with default values."""
     global _config
     if _config is None:
-        _config = default_config.DEFAULT_CONFIG.copy()
+        _config = deepcopy(default_config.DEFAULT_CONFIG)
 
 
 def set_config(config: Dict):
-    """Update the configuration with custom values."""
+    """Update the configuration with custom values.
+
+    Dict-valued keys (e.g. ``data_vendors``) are merged one level deep so a
+    partial update like ``{"data_vendors": {"core_stock_apis": "alpha_vantage"}}``
+    keeps the other nested keys from the default; scalar keys are replaced.
+    """
     global _config
-    if _config is None:
-        _config = default_config.DEFAULT_CONFIG.copy()
-    _config.update(config)
+    initialize_config()
+    incoming = deepcopy(config)
+    for key, value in incoming.items():
+        if isinstance(value, dict) and isinstance(_config.get(key), dict):
+            _config[key].update(value)
+        else:
+            _config[key] = value
 
 
 def get_config() -> Dict:
     """Get the current configuration."""
     if _config is None:
         initialize_config()
-    return _config.copy()
+    return deepcopy(_config)
 
 
 # Initialize with default config

skanga/TradingAgents

tradingagents/dataflows/config.py
@@ -1,4 +1,5 @@
 from contextvars import ContextVar
+from copy import deepcopy
 from typing import Dict
 
 import tradingagents.default_config as default_config
@@ -6,33 +7,41 @@ import tradingagents.default_config as default_config
 _config_var: ContextVar[dict | None] = ContextVar("tradingagents_config", default=None)
 
 
+def _merged_config(config: Dict) -> Dict:
+    """Merge config into defaults, preserving sibling nested keys."""
+    base = deepcopy(default_config.DEFAULT_CONFIG)
+    incoming = deepcopy(config)
+    for key, value in incoming.items():
+        if isinstance(value, dict) and isinstance(base.get(key), dict):
+            base[key].update(value)
+        else:
+            base[key] = value
+    return base
+
+
 def initialize_config():
     """Initialize the configuration with default values."""
     if _config_var.get() is None:
-        _config_var.set(default_config.DEFAULT_CONFIG.copy())
+        _config_var.set(deepcopy(default_config.DEFAULT_CONFIG))
 
 
 def set_config(config: Dict):
     """Set configuration for the current context."""
-    base = default_config.DEFAULT_CONFIG.copy()
-    base.update(config)
-    _config_var.set(base)
+    _config_var.set(_merged_config(config))
 
 
 def get_config() -> dict:
     """Get the current configuration."""
     cfg = _config_var.get()
     if cfg is None:
-        cfg = default_config.DEFAULT_CONFIG.copy()
+        cfg = deepcopy(default_config.DEFAULT_CONFIG)
         _config_var.set(cfg)
-    return cfg.copy()
+    return deepcopy(cfg)
 
 
 def use_config(config: Dict):
     """Apply configuration to the current context and return a reset token."""
-    base = default_config.DEFAULT_CONFIG.copy()
-    base.update(config)
-    return _config_var.set(base)
+    return _config_var.set(_merged_config(config))
 
 
 def reset_config(token) -> None: